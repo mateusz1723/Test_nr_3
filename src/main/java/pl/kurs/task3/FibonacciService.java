a8070c3bed867f0fb63d3f20bcfeb74238a9d3cd
mateusz.trochimowicz <trohimowicz@onet.pl>
2022-10-08 19:47:09 +0200

    Find fibonacci index

---
+++ src/main/java/pl/kurs/task3/FibonacciService.java
@@ -0,0 +1,32 @@
+package pl.kurs.task3;
+
+public class FibonacciService {
+    private int n = 3;
+
+    public FibonacciService() {
+    }
+
+    public int getFibonacciIndex(int number) {
+        if (number == 0)
+            return 0;
+        else if (number == 1)
+            return 1;
+
+        if (fib(n) == number)
+            return n;
+        else if (fib(n) > number)
+            return n = -1;
+        else fib(n++);
+
+        return getFibonacciIndex(number);
+    }
+
+    private int fib(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+}
---
+++ src/main/java/pl/kurs/task3/Runner.java
@@ -0,0 +1,13 @@
+package pl.kurs.task3;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        FibonacciService fibonacciService = new FibonacciService();
+
+        System.out.println(fibonacciService.getFibonacciIndex(89));
+
+    }
+
+
+}
